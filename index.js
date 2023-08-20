f74b34444bf7e466ab50b60dac2296b7367ce4bc
santoshi <santoshitolapi2000@gmail.com>
2023-08-20 14:28:55 +0530

    build the project

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// const heading=React.createElement("h1",{},"this is react")
+
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
