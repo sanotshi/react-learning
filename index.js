894a137500bb8c3ec8c139ca65a07f58eb755cd6
santoshi <santoshitolapi2000@gmail.com>
2023-08-20 14:42:44 +0530

    render variable into react functional component

--- index.js
+++ index.js
@@ -4,5 +4,15 @@ import ReactDOM from "react-dom";
 // const heading=React.createElement("h1",{},"this is react")
 
 
+const Title=()=>(
+    <h1 className="head">namaste react</h1>
+);
+
+const HeadingComponent=()=>(
+    <div id="container">
+           <Title />
+           <h1 className="heading">this is react functional component</h1>
+    </div>
+);
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent />);
