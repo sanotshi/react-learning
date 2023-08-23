07089005fa94b6477b79cbb28cbee6fa08b1e484
santoshi <santoshitolapi2000@gmail.com>
2023-08-23 22:08:48 +0530

    add gitignore

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Assignment } from "./src/assignment";
 
-// const heading=React.createElement("h1",{},"this is react")
-
-
-const Title=()=>(
-    <h1 className="head">namaste react</h1>
-);
 const number="hello";
-const HeadingComponent=()=>(
-    <div id="container">
-        {number}
-           <Title />
-           <Title></Title>
-           {Title()}
-           <h1 className="heading">this is react functional component</h1>
-    </div>
+const Index=()=>(
+    <Assignment/>
 );
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<Index />);
