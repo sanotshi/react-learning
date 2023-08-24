6da453be0c9237f838d68547fd257037760d8a9c
santoshi <santoshitolapi2000@gmail.com>
2023-08-24 15:58:02 +0530

    create new project food-app

---
+++ src1/food-app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ const FoodApp=()=>{
+    return (
+        <div className="head">
+            <h1>hello</h1>
+        </div>
+    );
+}
+export default FoodApp;
+//  const root=ReactDOM.createRoot(document.getElementById("root"));
+//  root.render(<FoodApp />);
