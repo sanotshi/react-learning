333570674810011cd8777dae41d4aa6efe27ab9b
santoshi <santoshitolapi2000@gmail.com>
2023-08-26 12:38:56 +0530

    fetch github userInfo

--- src1/components/UserClass.js
+++ src1/components/UserClass.js
@@ -1,14 +1,42 @@
 import React from "react";
 
 class UserClass extends React.Component{
-    constructor(props){
+     constructor(props){
     super(props)
-    console.log(props)
+     console.log(props)
+     this.state={
+        userInfo:{
+            name:"Dummy",
+
+            location:"Default",
+        },
+     };
+     }
+
+    async componentDidMount(){
+        const data=await fetch("https://api.github.com/users/SantoshiTolapi");
+        const json=await data.json();
+        this.setState({
+            userInfo:json,
+        })
+        console.log(json);
     }
     render(){
+        const{count,count1}=this.state;
+        const {login,id}=this.state.userInfo;
         return(
             <div className="User-card">
                 <h1>this is class based component</h1>
+                {/* <h2>count:{count}</h2>
+                <button onClick={()=>{
+                    this.setState({
+                        count:this.state.count+1,
+                        count1:this.state.count1+1,
+                    })
+                }}>count increase</button> */}
+                <h2>count1:{count1}</h2>
+                <h2>name:{login}</h2>
+                <h3>location:{id}</h3>
             </div>
         )
     }
